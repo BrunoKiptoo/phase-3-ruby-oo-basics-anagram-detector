def60aaac44a2548ec8745889edba5d570b6bfb8
Bruno <brunochelimo@gmail.com>
2023-02-13 12:21:44 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| w.chars.sort == @word.chars.sort && w.downcase != @word.downcase }
+    end
+  end
+  
